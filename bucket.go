92e7fba6044a9b09e45ea42b4f3be467b5bbf426
valsov <valentin.sovignet@sfr.fr>
2023-11-26 11:44:31 +0100

    Bucket limiter implementation

--- bucket.go
+++ bucket.go
@@ -7,8 +7,9 @@ import (
 )
 
 type BucketConfig struct {
-	RefillRate time.Duration
-	BucketSize int
+	RefillRate  time.Duration
+	RefillCount int // How much tokens should be periodically added to the bucket
+	BucketSize  int
 }
 
 type BucketValue struct {
@@ -25,6 +26,23 @@ func NewBucketLimiter(storageProvider storage.Storage[BucketConfig, BucketValue]
 	}
 }
 
-func (b *BucketLimiter) TryAllow(count int, config BucketConfig, userValue BucketValue, now time.Time) bool {
-	panic("todo")
+func (b *BucketLimiter) TryAllow(count int, config BucketConfig, userValue BucketValue, nowUtc time.Time) (bool, BucketValue) {
+	// Compute current bucket fill
+	sinceLastRefill := nowUtc.Sub(userValue.LastRefillUtc)
+	refillTimes := int(sinceLastRefill.Seconds() / config.RefillRate.Seconds())
+	newFill := userValue.RemainingTokens + refillTimes*config.RefillCount
+	if newFill > config.BucketSize {
+		newFill = config.BucketSize
+	}
+
+	// Try allow count
+	allow := newFill >= count
+	if allow {
+		newFill -= count
+	}
+
+	// Update user value
+	userValue.RemainingTokens = newFill
+	userValue.LastRefillUtc = userValue.LastRefillUtc.Add(time.Duration(refillTimes) * config.RefillRate) // Not nowUtc because it would make the refill time drift
+	return allow, userValue
 }
---
+++ bucket_test.go
@@ -0,0 +1,79 @@
+package rlimit
+
+import (
+	"testing"
+	"time"
+)
+
+func TestTryAllow(t *testing.T) {
+	config := BucketConfig{
+		RefillRate:  time.Hour,
+		RefillCount: 10,
+		BucketSize:  15,
+	}
+	userValue := BucketValue{
+		LastRefillUtc:   time.Now().UTC(),
+		RemainingTokens: 15,
+	}
+
+	testCases := []struct {
+		count                   int
+		nowUtc                  time.Time
+		expectedAllow           bool
+		expectedLastRefillUtc   time.Time
+		expectedRemainingTokens int
+	}{
+		{
+			count:                   1,
+			nowUtc:                  userValue.LastRefillUtc.Add(10 * time.Minute),
+			expectedAllow:           true,
+			expectedLastRefillUtc:   userValue.LastRefillUtc,
+			expectedRemainingTokens: 14,
+		},
+		{
+			count:                   14,
+			nowUtc:                  userValue.LastRefillUtc.Add(20 * time.Minute),
+			expectedAllow:           true,
+			expectedLastRefillUtc:   userValue.LastRefillUtc,
+			expectedRemainingTokens: 0,
+		},
+		{
+			count:                   1,
+			nowUtc:                  userValue.LastRefillUtc.Add(30 * time.Minute),
+			expectedAllow:           false,
+			expectedLastRefillUtc:   userValue.LastRefillUtc,
+			expectedRemainingTokens: 0,
+		},
+		{
+			count:                   1,
+			nowUtc:                  userValue.LastRefillUtc.Add(time.Hour),
+			expectedAllow:           true,
+			expectedLastRefillUtc:   userValue.LastRefillUtc.Add(time.Hour),
+			expectedRemainingTokens: 9, // RefillCount is only 10 while BucketSize is 15
+		},
+		{
+			count:                   1,
+			nowUtc:                  userValue.LastRefillUtc.Add(2 * time.Hour),
+			expectedAllow:           true,
+			expectedLastRefillUtc:   userValue.LastRefillUtc.Add(2 * time.Hour),
+			expectedRemainingTokens: 14, // Fully refilled but allow 1
+		},
+	}
+	for _, tc := range testCases {
+		limiter := BucketLimiter{}
+		result, newUserValue := limiter.TryAllow(tc.count, config, userValue, tc.nowUtc)
+
+		if result != tc.expectedAllow {
+			t.Errorf("wrong result. expected=%v got=%v", tc.expectedAllow, result)
+		}
+		if newUserValue.LastRefillUtc != tc.expectedLastRefillUtc {
+			t.Errorf("wrong userValue.LastRefillUtc. expected=%v got=%v", tc.expectedLastRefillUtc, userValue.LastRefillUtc)
+		}
+		if newUserValue.RemainingTokens != tc.expectedRemainingTokens {
+			t.Errorf("wrong userValue.RemainingTokens. expected=%d got=%d", tc.expectedRemainingTokens, userValue.RemainingTokens)
+		}
+
+		// Update user value for next test cases
+		userValue = newUserValue
+	}
+}
